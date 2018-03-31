6510e3b9f36d33a404baab4b1ee97935836356ab
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-03-30 21:10:54 -0700

    improved seeds

--- seeds.sql
+++ seeds.sql
@@ -1,30 +1,33 @@
-/* category options for games:
+INSERT INTO category (name)
+VALUES ("Family"), ("Kids"), ("Adults"), ("Party"), ("Card Game"), ("Dice"), ("Educational"), ("Memory Game"), ("Word Game"), ("Dexterity"), ("Humor"), ("Math"), ("Military"), ("Strategy"), ("Puzzle"), ("Miniatures");
 
-family
-kids
-adults
-party
-card game
-dice
-educational
-memory_game
-word_game
-dexterity
-humor
-math
-military
-strategy
-puzzle
-miniatures */
+/*
+1	Family
+2	Kids
+3	Adults
+4	Party
+5	Card Game
+6	Dice
+7	Educational
+8	Memory Game
+9	Word Game
+10	Dexterity
+11	Humor
+12	Math
+13	Military
+14	Strategy
+15	Puzzle
+16	Miniatures
+*/
 
+INSERT INTO age_range (range)
+VALUES ("Less than 8 years old"), ("8 - 13 years old"), ("13 Years and Up"), ("Adult Games Only");
 
-/* options for age ranges:
-
-Less than 8 years old
-8 - 13 years old
-13 Years and Up
-Adult Games Only
-
+/*
+1	Less than 8 years old
+2	8 - 13 years old
+3	13 Years and Up
+4	Adult Games Only
 */
 
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
@@ -33,49 +36,19 @@ VALUES ("Candy Land", 1, 1, "Simple", 2, 4, 30, "Hasbro", 1949, "https://cdn.sho
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
 VALUES ("Candy Land", 2, 1, "Simple", 2, 4, 30, "Hasbro", 1949,  "https://cdn.shopify.com/s/files/1/1911/5793/products/Candy-Land-Content_1024x1024.jpg?v=1512062059");
 
-INSERT INTO category (name)
-VALUES ("Family");
-
-INSERT INTO category (name)
-VALUES ("Kids");
-
-INSERT INTO age_range (range)
-VALUES ("Less than 8 years old");
-
-
-INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
-VALUES ("Monopoly", 1, 1, "Intermediate", 2, 8, 180, "Hasbro", 1935,"https://upload.wikimedia.org/wikipedia/commons/d/df/German_Monopoly_board_in_the_middle_of_a_game.jpg");
 
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
 VALUES ("Monopoly", 1, 2, "Intermediate", 2, 8, 180, "Hasbro", 1935,"https://upload.wikimedia.org/wikipedia/commons/d/df/German_Monopoly_board_in_the_middle_of_a_game.jpg");
 
-INSERT INTO category (name)
-VALUES ("Family");
-
-INSERT INTO age_range (range)
-VALUES ("8 - 13 years old");
-
-INSERT INTO age_range (range)
-VALUES ("13 Years and Up");
+INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
+VALUES ("Monopoly", 1, 3, "Intermediate", 2, 8, 180, "Hasbro", 1935,"https://upload.wikimedia.org/wikipedia/commons/d/df/German_Monopoly_board_in_the_middle_of_a_game.jpg");
 
 
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
-VALUES ("Scrabble", 1, 1, "Intermediate", 2, 4, 90, "Hasbro", 1938, "https://upload.wikimedia.org/wikipedia/commons/5/5d/Scrabble_game_in_progress.jpg");
+VALUES ("Scrabble", 1, 2, "Intermediate", 2, 4, 90, "Hasbro", 1938, "https://upload.wikimedia.org/wikipedia/commons/5/5d/Scrabble_game_in_progress.jpg");
 
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
-VALUES ("Scrabble", 2, 2, "Intermediate", 2, 4, 90, "Hasbro", 1938, "https://upload.wikimedia.org/wikipedia/commons/5/5d/Scrabble_game_in_progress.jpg");
-
-INSERT INTO category (name)
-VALUES ("Family");
-
-INSERT INTO category (name)
-VALUES ("Word Game");
-
-INSERT INTO age_range (range)
-VALUES ("8 - 13 years old");
-
-INSERT INTO age_range (range)
-VALUES ("13 years and up");
+VALUES ("Scrabble", 9, 3, "Intermediate", 2, 4, 90, "Hasbro", 1938, "https://upload.wikimedia.org/wikipedia/commons/5/5d/Scrabble_game_in_progress.jpg");
 
 
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
@@ -84,14 +57,6 @@ VALUES ("Twister", 1, 1, "Simple", 2, 4, 10, "Hasbro", 1966, "https://cdn.shopif
 INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
 VALUES ("Twister", 2, 2, "Simple", 2, 4, 10, "Hasbro", 1966,  "https://cdn.shopify.com/s/files/1/1911/5793/products/HG98831lg_1024x1024.jpg?v=1506010693");
 
-INSERT INTO category (name)
-VALUES ("Party");
+INSERT INTO boardGames (gameName, category_id, age_id, difficulty, minPlayer, maxPlayer, timeToPlay, manufacturer, year, img) 
+VALUES ("Twister", 10, 3, "Simple", 2, 4, 10, "Hasbro", 1966,  "https://cdn.shopify.com/s/files/1/1911/5793/products/HG98831lg_1024x1024.jpg?v=1506010693");
 
-INSERT INTO category (name)
-VALUES ("Dexterity");
-
-INSERT INTO age_range (range)
-VALUES ("8 - 13 years old");
-
-INSERT INTO age_range (range)
-VALUES ("13 Years and Up");
