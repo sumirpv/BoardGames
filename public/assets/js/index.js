16ba7695df9e3be285c4a1249f1ec9d0ab890c52
Brenton Jenkins <brentonjjenkins@gmail.com>
2018-03-31 18:02:58 -0700

    html1

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -14,8 +14,7 @@ $(document).ready(function() {
             category: $("#category").val().trim(),
             numPlayers: $("#num-players").val().trim(),
             age: $("#age").val().trim(),
-            timeToPlay: $("#time-play").val().trim(),
-            timetolearn: $("#time-learn").val().trim()
+            timeToPlay: $("#time-play").val().trim()
         }
 
         // console.log(newGameSearch);
@@ -34,15 +33,13 @@ $(document).ready(function() {
                 console.log("Suggested Age Range: " + data[i].age_range);
                 console.log("Minimum Players Needed: " + data[i].minPlayer);
                 console.log("Maximum Players Needed: " + data[i].maxPlayer);
-                console.log("Avg. Time Needed to Play (est.): " + data[i].timeToPlay);
-                // console.log("Avg Time to Learn (est.): " + data[i].timetolearn);
+                console.log("Average Time Needed to Play: " + data[i].timeToPlay);
                 
                 chosenGame.append("Game Name: " + data[i].gameName + "<br>");
                 chosenGame.append("Suggested Minimum Age??" + data[i].age_range + "<br>");
                 chosenGame.append("Minimum Players: " + data[i].minPlayer + "<br>");
                 chosenGame.append("Maximum Players: " + data[i].maxPlayer + "<br>");
-                chosenGame.append("Avg. Time Needed to Play (est.): " + data[i].timeToPlay + "<br>");
-                // chosenGame.append("Avg Time to Learn (est.): " + data[i].timetolearn + "<br><br>");
+                chosenGame.append("Average Time Needed to Play: " + data[i].timeToPlay + "<br>");
 
                 $(".game-area").append(chosenGame);
 
